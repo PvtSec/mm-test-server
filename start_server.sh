7d784f83f03e4d5008c2d2f5633ee029d1691964
akshay <akssofts@gmail.com>
2024-02-08 19:44:03 +0530

    Chore: fix working dir

--- start_server.sh
+++ start_server.sh
@@ -10,7 +10,6 @@ chown -R postgres:postgres /var/lib/postgresql
 
 su postgres -c "/usr/lib/postgresql/14/bin/initdb -D $PGDATA"
 su postgres -c "/usr/lib/postgresql/14/bin/pg_ctl -D $PGDATA start"
-su postgres -c "createdb mattermost"
 su postgres -c "psql -c \"CREATE DATABASE mattermost;\""
 su postgres -c "psql -c \"CREATE USER mmuser WITH PASSWORD 'mmpassword';\""
 su postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE mattermost TO mmuser;\""
